526514d22b8a9f41541099cc192ba9343373bd15
ShammiL <shammi0107@gmail.com>
2025-08-19 09:52:30 +0530

    Update the mi-module-gen command

--- tool-mi-module-gen-cli/src/main/java/io/ballerina/mi/cmd/MiCmd.java
+++ tool-mi-module-gen-cli/src/main/java/io/ballerina/mi/cmd/MiCmd.java
@@ -19,8 +19,8 @@
 package io.ballerina.mi.cmd;
 
 import io.ballerina.cli.BLauncherCmd;
-import io.ballerina.projects.JBallerinaBackend;
-import io.ballerina.projects.JvmTarget;
+import io.ballerina.projects.*;
+import io.ballerina.projects.Module;
 import io.ballerina.projects.Package;
 import io.ballerina.projects.PackageCompilation;
 import io.ballerina.projects.Project;
@@ -37,16 +37,26 @@ import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.Objects;
+import java.util.UUID;
 
 @CommandLine.Command(name = "mi-module-gen", description = "Generate WSO2 MI module")
 public class MiCmd implements BLauncherCmd {
     private static final String CMD_NAME = "mi-module-gen";
+    private static final String CONNECTOR_TARGET_PATH = "CONNECTOR_TARGET_PATH";
+    private static final String CONNECTOR_NAME_SEPARATOR = "-";
+    private static final String MI_IMPORT_FILE = "mi_import_file";
+    private static final String MI_IMPORT = "import wso2/mi as _;" + System.lineSeparator();
     private final PrintStream printStream;
     @CommandLine.Option(names = {"--help", "-h"}, usageHelp = true)
     private boolean helpFlag;
     @CommandLine.Option(names = {"--input", "-i"}, description = "Ballerina project path")
     private String sourcePath;
+    @CommandLine.Option(names = {"--connector", "-c"}, description = "Generate MI module for Ballerina connector")
+    private String targetPath;
+    Path executablePath;
 
     public MiCmd() {
         this.printStream = System.out;
@@ -54,6 +64,7 @@ public class MiCmd implements BLauncherCmd {
 
     @Override
     public void execute() {
+        String miImportDocumentName;
         if (sourcePath == null || helpFlag) {
             StringBuilder stringBuilder = new StringBuilder();
             setHelpMessage(stringBuilder);
@@ -62,22 +73,65 @@ public class MiCmd implements BLauncherCmd {
         }
 
         Path path = Path.of(sourcePath).normalize();
-        Project project = ProjectLoader.loadProject(path);
-        Package pkg = project.currentPackage();
-        PackageCompilation packageCompilation = pkg.getCompilation();
+        BuildOptions buildOptions = BuildOptions.builder().setSticky(false).setOffline(false).build();
+        Project project = ProjectLoader.loadProject(path, buildOptions);
+        Package pkgBeforeImportAdded = project.currentPackage();
+
+        if (null != targetPath) {
+            Path miConnectorCache = Paths.get(targetPath, "BalConnectors");
+            executablePath = miConnectorCache.resolve(pkgBeforeImportAdded.descriptor().org().value() +
+                    CONNECTOR_NAME_SEPARATOR + pkgBeforeImportAdded.descriptor().name().value() +
+                    CONNECTOR_NAME_SEPARATOR + pkgBeforeImportAdded.descriptor().version().toString() + ".jar" );
+            if (Files.exists(executablePath)) {
+                // Executable for the specific connector version will not be created if it already exists
+                //TODO: Check if the ZIP exists as well before quiting
+                printStream.println("executable already exists");
+                return;
+            }
+            try {
+                Files.createDirectories(miConnectorCache);
+            } catch (IOException e) {
+                throw  new RuntimeException(e);
+            }
+            System.setProperty(CONNECTOR_TARGET_PATH, executablePath.toString());
+        } else {
+            Path bin = path.resolve("target").resolve("bin");
+            try {
+                createBinFolder(bin);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            executablePath = bin.resolve(pkgBeforeImportAdded.descriptor().name().value() + ".jar");
+        }
+//        TODO: REMOVE-if DOCUMENT adding is not needed
+        // Add new Document with import
+        Module defaultModule = project.currentPackage().getDefaultModule();
+        List<String> defaultModuleFiles = defaultModule.documentIds()
+                .stream()
+                .map(documentId -> defaultModule.document(documentId).name())
+                .toList();
+        do {
+            miImportDocumentName = String.format("%s-%s.bal", MI_IMPORT_FILE, UUID.randomUUID());
+        } while (defaultModuleFiles.contains(miImportDocumentName));
+
+        ModuleId defaultModuleId = defaultModule.moduleId();
+        DocumentId documentId = DocumentId.create(miImportDocumentName, defaultModuleId);
+        DocumentConfig documentConfig = DocumentConfig.from(documentId, MI_IMPORT,
+                miImportDocumentName);
+        defaultModule.modify().addDocument(documentConfig).apply();
+
+        Package pkgAfterImportAdded = project.currentPackage();
+        PackageCompilation packageCompilation = pkgAfterImportAdded.getCompilation();
+        // TODO: FIX-below only prints a single diagnostic before returning
         for (Diagnostic diagnostic : packageCompilation.diagnosticResult().diagnostics()) {
             printStream.println(diagnostic.toString());
             return;
         }
+//        TODO: CHECK-How to determine the JvmTarget, does it have any connection with the connector platform?
         JBallerinaBackend jBallerinaBackend = JBallerinaBackend.from(packageCompilation, JvmTarget.JAVA_21);
-        Path bin = path.resolve("target").resolve("bin");
-        try {
-            createBinFolder(bin);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        Path executablePath = bin.resolve(pkg.descriptor().name().value() + ".jar");
+
         jBallerinaBackend.emit(JBallerinaBackend.OutputType.EXEC, executablePath);
+        // TODO: FIX-check emit result diagnostics and conflicting jars and remove if the below is not needed
         if (packageCompilation.diagnosticResult().diagnosticCount() > 0) {
             printStream.println("Errors in compiling Ballerina project");
         }
