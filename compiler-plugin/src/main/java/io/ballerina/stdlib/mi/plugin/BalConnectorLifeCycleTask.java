9761f6667d5ada7ce85185d0d5242cb0bbae1674
ShammiL <shammi0107@gmail.com>
2025-09-29 11:31:13 +0530

    Update JSON and XML generation in lifecycle task

--- compiler-plugin/src/main/java/io/ballerina/stdlib/mi/plugin/BalConnectorLifeCycleTask.java
+++ compiler-plugin/src/main/java/io/ballerina/stdlib/mi/plugin/BalConnectorLifeCycleTask.java
@@ -1,10 +1,30 @@
+/*
+ * Copyright (c) 2025, WSO2 LLC. (https://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package io.ballerina.stdlib.mi.plugin;
 
 import io.ballerina.projects.PackageDescriptor;
 import io.ballerina.projects.plugins.CompilerLifecycleEventContext;
 import io.ballerina.projects.plugins.CompilerLifecycleTask;
 import io.ballerina.stdlib.mi.plugin.connectorModel.Component;
+import io.ballerina.stdlib.mi.plugin.connectorModel.Connection;
 import io.ballerina.stdlib.mi.plugin.connectorModel.Connector;
+import io.ballerina.stdlib.mi.plugin.connectorModel.FunctionType;
 
 import java.io.File;
 import java.io.IOException;
@@ -26,9 +46,7 @@ public class BalConnectorLifeCycleTask implements CompilerLifecycleTask<Compiler
 
         PackageDescriptor descriptor = context.currentPackage().manifest().descriptor();
 
-        Connector connector = Connector.getConnector();
-        // TODO: Remove one of module name and name
-        connector.setName(descriptor.name().value());
+        Connector connector = Connector.getConnector(descriptor);
         connector.setVersion(descriptor.version().value().toString());
 
         try {
@@ -38,7 +56,7 @@ public class BalConnectorLifeCycleTask implements CompilerLifecycleTask<Compiler
             generateJsonFiles(destinationPath, connector);
             URI jarPath = getClass().getProtectionDomain().getCodeSource().getLocation().toURI();
             Utils.copyResources(getClass().getClassLoader(), destinationPath, jarPath, connector.getOrgName(),
-                    connector.getModuleName(), connector.getModuleVersion());
+                    connector.getModuleName(), connector.getMajorVersion());
             Files.copy(generatedArtifactPath, destinationPath.resolve(Connector.LIB_PATH).resolve(generatedArtifactPath.getFileName()));
             String zipFilePath = generatedArtifactPath.getParent().getParent().toString() + File.separator + "Downloaded" + File.separator + connector.getZipFileName();
             Utils.zipFolder(destinationPath, zipFilePath);
@@ -53,15 +71,16 @@ public class BalConnectorLifeCycleTask implements CompilerLifecycleTask<Compiler
         if (!connectorFolder.exists()) {
             connectorFolder.mkdir();
         }
-
+        // Generate the connector.xml
         connector.generateInstanceXml(connectorFolder);
+        // Generate the component.xml for functions
         connector.generateFunctionsXml(connectorFolder, FUNCTION_TEMPLATE_PATH, "functions");
-
-        for (Component component : connector.getComponents()) {
-            if (component.getIsConfig()) {
-                component.generateInstanceXml(connectorFolder, CONFIG_TEMPLATE_PATH, "config");
-                component.generateTemplateXml(connectorFolder, CONFIG_TEMPLATE_PATH, "config");
-            } else {
+        // Generate the config/component.xml
+        connector.generateConfigInstanceXml(connectorFolder, CONFIG_TEMPLATE_PATH, "config");
+        // Generate the init.xml from config_template.xml
+        connector.generateConfigTemplateXml(connectorFolder, CONFIG_TEMPLATE_PATH, "config");
+        for (Connection connection: connector.getConnections()) {
+            for (Component component : connection.getComponents()) {
                 component.generateTemplateXml(connectorFolder, FUNCTION_TEMPLATE_PATH, "functions");
             }
         }
@@ -69,13 +88,14 @@ public class BalConnectorLifeCycleTask implements CompilerLifecycleTask<Compiler
 
     private static void generateJsonFiles(Path connectorFolderPath, Connector connector) {
         File connectorFolder = new File(connectorFolderPath.toUri());
-        for (Component component : connector.getComponents()) {
-            if (component.getIsConfig()) {
-                component.generateUIJson(connectorFolder, CONFIG_TEMPLATE_PATH, connector.getName());
-            }
-            component.generateUIJson(connectorFolder, FUNCTION_TEMPLATE_PATH, component.getName());
-            //TODO: Generate output schemas
+        for (Connection connection: connector.getConnections()) {
+            connection.getInitComponent().generateUIJson(connectorFolder, CONFIG_TEMPLATE_PATH,
+                    connection.getConnectionType());
+            for (Component component : connection.getComponents()) {
+                component.generateUIJson(connectorFolder, FUNCTION_TEMPLATE_PATH, component.getName());
+                //TODO: Generate output schemas
 //            component.generateOutputSchemaJson(connectorFolder);
+            }
         }
     }
 }
