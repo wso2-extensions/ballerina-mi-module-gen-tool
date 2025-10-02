37c115038f68f6777fc7eaf75ff4c15910d4765e
ShammiL <shammi0107@gmail.com>
2025-10-03 00:45:48 +0530

    Introduce constants

--- compiler-plugin/src/main/java/io/ballerina/stdlib/mi/plugin/Constants.java
+++ compiler-plugin/src/main/java/io/ballerina/stdlib/mi/plugin/Constants.java
@@ -3,6 +3,7 @@ package io.ballerina.stdlib.mi.plugin;
 import java.io.File;
 
 public class Constants {
+    public static final String INIT_FUNCTION_NAME = "init";
     public static final String BOOLEAN = "boolean";
     public static final String INT = "int";
     public static final String STRING = "string";
@@ -13,17 +14,22 @@ public class Constants {
     public static final String UNION = "union";
     public static final String RECORD = "record";
     public static final String ARRAY = "array";
-    public static final String RECORD_FIELD = "field";
-    public static final String INTERSECTION = "intersection";
     public static final String ENUM = "enum";
     public static final String FUNCTION_TEMPLATE_PATH = "balConnector" + File.separator + "functions";
     public static final String JSON_TEMPLATE_PATH = "balConnector" + File.separator + "inputTemplates";
-    public static final String ATTRIBUTE_GROUP_TEMPLATE_PATH = JSON_TEMPLATE_PATH + File.separator + "attributeGroup.txt";
-    public static final String ATTRIBUTE_TEMPLATE_PATH = JSON_TEMPLATE_PATH + File.separator + "attribute.txt";
-    public static final String COMBO_ATTRIBUTE_TEMPLATE_PATH = JSON_TEMPLATE_PATH + File.separator + "comboAttribute.txt";
-    public static final String INPUT_TYPE_STRING = "string";
+    public static final String ATTRIBUTE_GROUP_TEMPLATE_PATH = JSON_TEMPLATE_PATH + File.separator
+            + "attributeGroup.json";
+    public static final String ATTRIBUTE_TEMPLATE_PATH = JSON_TEMPLATE_PATH + File.separator + "attribute.json";
+    public static final String COMBO_TEMPLATE_PATH = JSON_TEMPLATE_PATH + File.separator
+            + "combo.json";
+    public static final String TABLE_TEMPLATE_PATH = JSON_TEMPLATE_PATH + File.separator + "table.json";
+    public static final String INPUT_TYPE_STRING_OR_EXPRESSION = "stringOrExpression";
     public static final String INPUT_TYPE_BOOLEAN = "boolean";
     public static final String INPUT_TYPE_COMBO = "combo";
-    public static final String INIT_FUNCTION_NAME = "init";
+    public static final String VALIDATE_TYPE_REGEX = "regex";
+    public static final String INTEGER_REGEX = "^(-?\\d+|\\$\\{.+\\})$";
+    public static final String DECIMAL_REGEX = "^(-?\\d+(\\.\\d+)?|\\$\\{.+\\})$";
+    public static final String ATTRIBUTE_SEPARATOR = ",";
+    public static final String ATTRIBUTE_GROUP_END = "\n]\n}\n}\n";
 
 }
